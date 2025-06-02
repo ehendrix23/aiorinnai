57330b293fd8adc0a413a29ad9ea27b50b85fc8d
ehendrix23 <hendrix_erik@hotmail.com>
2025-06-02 13:37:22 -0600

    Increase version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name="aiorinnai",
-    version="0.4.0a2.dev1",
+    version="0.4.0a2.dev2",
     description="Python interface for Rinnai Control-R API",
     long_description=long_description,
     long_description_content_type='text/markdown',
